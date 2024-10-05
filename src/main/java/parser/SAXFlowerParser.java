478ed551bec40c3ae6596e04c182f4d54e9b6472
57459N <stasun008@gmail.com>
2024-10-06 00:26:33 +0300

    sax

---
+++ src/main/java/parser/SAXFlowerParser.java
@@ -0,0 +1,108 @@
+package parser;
+
+import flower.Flower;
+import flower.GrowingTips;
+import flower.VisualParameters;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class SAXFlowerParser implements FlowerParser {
+
+    private static final Logger logger = LogManager.getLogger(SAXFlowerParser.class);
+
+    @Override
+    public List<Flower> parse(String filePath) throws RuntimeException, FileNotFoundException {
+        List<Flower> flowers = new ArrayList<>();
+        try {
+            logger.info("Processing file: {}", filePath);
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            SAXParser saxParser = factory.newSAXParser();
+            saxParser.parse(new File(filePath), new FlowerHandler(flowers));
+            logger.info("Successfully parsed {} flowers", flowers.size());
+        } catch (FileNotFoundException e) {
+            logger.error("File not found: {}", filePath);
+            throw new FileNotFoundException(e.getMessage());
+        } catch (Exception e) {
+            logger.error("Error while parsing {}: {}", filePath, e.getMessage());
+            throw new RuntimeException(e);
+        }
+        return flowers;
+    }
+
+    private static class FlowerHandler extends DefaultHandler {
+        private static final String FLOWER_TAG = "Flower";
+        private static final String NAME_TAG = "Name";
+        private static final String SOIL_TAG = "Soil";
+        private static final String ORIGIN_TAG = "Origin";
+        private static final String PARAMETER_TAG = "Parameter";
+        private static final String TIP_TAG = "Tip";
+        private static final String MULTIPLYING_TAG = "Multiplying";
+
+        private final List<Flower> flowers;
+        private Flower currentFlower;
+        private StringBuilder currentValue;
+        private String currentParameterType;
+        private String currentTipType;
+
+        public FlowerHandler(List<Flower> flowers) {
+            this.flowers = flowers;
+        }
+
+        @Override
+        public void startElement(String uri, String localName, String qName, Attributes attributes) throws SAXException {
+            if (FLOWER_TAG.equals(qName)) {
+                String id = attributes.getValue("id");
+                currentFlower = new Flower(id, "", "", "", new VisualParameters(), new GrowingTips(), "");
+                logger.debug("Parsing flower: {}", id);
+            } else if (PARAMETER_TAG.equals(qName)) {
+                currentParameterType = attributes.getValue("type");
+            } else if (TIP_TAG.equals(qName)) {
+                currentTipType = attributes.getValue("type");
+            }
+            currentValue = new StringBuilder();
+        }
+
+        @Override
+        public void characters(char[] ch, int start, int length) throws SAXException {
+            currentValue.append(ch, start, length);
+        }
+
+        @Override
+        public void endElement(String uri, String localName, String qName) throws SAXException {
+            if (FLOWER_TAG.equals(qName)) {
+                flowers.add(currentFlower);
+            } else if (NAME_TAG.equals(qName)) {
+                currentFlower.setName(currentValue.toString().trim());
+            } else if (SOIL_TAG.equals(qName)) {
+                currentFlower.setSoil(currentValue.toString().trim());
+            } else if (ORIGIN_TAG.equals(qName)) {
+                currentFlower.setOrigin(currentValue.toString().trim());
+            } else if (PARAMETER_TAG.equals(qName) && currentFlower != null) {
+                String value = currentValue.toString().trim();
+                currentFlower.getVisualParameters().addParameter(currentParameterType, value);
+            } else if (TIP_TAG.equals(qName) && currentFlower != null) {
+                String value = currentValue.toString().trim();
+                currentFlower.getGrowingTips().addTip(currentTipType, value);
+            } else if (MULTIPLYING_TAG.equals(qName)) {
+                currentFlower.setMultiplying(currentValue.toString().trim());
+            }
+
+            if (PARAMETER_TAG.equals(qName)) {
+                currentParameterType = null;
+            } else if (TIP_TAG.equals(qName)) {
+                currentTipType = null;
+            }
+        }
+    }
+
+}
---
+++ src/test/java/test/SAXFlowerParserTest.java
@@ -0,0 +1,52 @@
+package test;
+
+import flower.Flower;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import parser.SAXFlowerParser;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.List;
+
+public class SAXFlowerParserTest {
+
+    private SAXFlowerParser saxFlowerParser;
+    private String xmlFilePath;
+
+    @BeforeClass
+    public void setUp() {
+        saxFlowerParser = new SAXFlowerParser();
+        xmlFilePath = "src/main/resources/flowers.xml";
+    }
+
+    @Test
+    public void testParseFlowers() throws FileNotFoundException {
+        List<Flower> flowers = saxFlowerParser.parse(xmlFilePath);
+        Assert.assertNotNull(flowers, "Parsed flowers list should not be null");
+        Assert.assertFalse(flowers.isEmpty(), "Parsed flowers list should not be empty");
+        Assert.assertEquals(flowers.size(), 16, "Expected number of flowers should be 16");
+
+        Flower firstFlower = flowers.get(0);
+        Assert.assertEquals(firstFlower.getName(), "Роза", "First flower name should be Роза");
+        Assert.assertEquals(firstFlower.getSoil(), "Грунтовая", "First flower soil should be Грунтовая");
+        Assert.assertEquals(firstFlower.getOrigin(), "Иран", "First flower origin should be Иран");
+        Assert.assertEquals(firstFlower.getMultiplying(), "Черенками", "First flower multiplying should be Черенками");
+        Assert.assertEquals(firstFlower.getVisualParameters().getParameter("stemColor"), "Красный", "Stem color should be Красный");
+
+        Flower secondFlower = flowers.get(1);
+        Assert.assertEquals(secondFlower.getName(), "Тюльпан", "Second flower name should be Тюльпан");
+        Assert.assertEquals(secondFlower.getSoil(), "Дерново-подзолистая", "Second flower soil should be Дерново-подзолистая");
+        Assert.assertEquals(secondFlower.getOrigin(), "Турция", "Second flower origin should be Турция");
+        Assert.assertEquals(secondFlower.getMultiplying(), "Семена", "Second flower multiplying should be Семена");
+        Assert.assertEquals(secondFlower.getVisualParameters().getParameter("stemColor"), "Желтый", "Stem color should be Желтый");
+    }
+
+    @Test
+    public void testParseInvalidFile() {
+        String invalidXmlFilePath = "invalid_flowers.xml";
+        Assert.assertThrows(FileNotFoundException.class, () -> saxFlowerParser.parse(invalidXmlFilePath));
+    }
+}
+
